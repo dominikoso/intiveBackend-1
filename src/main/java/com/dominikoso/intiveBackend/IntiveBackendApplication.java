b87bb93315372c3ce23693e18cd67d95256f1d0b
Dominik K <rodzinka.kostkow@gmail.com>
2019-01-02 16:03:27 +0100

    Initial commit

---
+++ src/main/java/com/dominikoso/intiveBackend/IntiveBackendApplication.java
@@ -0,0 +1,14 @@
+package com.dominikoso.intiveBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntiveBackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dominikoso/intiveBackend/IntiveBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dominikoso.intiveBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntiveBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
